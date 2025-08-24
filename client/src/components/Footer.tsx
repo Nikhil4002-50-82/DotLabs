da9d0ed2e732f4a96cb57758160c1e34a88798f9
Nikhil <nikhil.4002.50.82@gmail.com>
2025-08-24 12:13:22 +0530

    modified client

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="relative bottom-0 px-[2em] py-[1.5em] shadow-custom flex items-center justify-between">
+    <div className="relative bottom-0 px-[2.5em] py-[1.5em] shadow-custom flex items-center justify-between">
       <h3 className="font-DotLabs text-lg font-semibold">DotLabs</h3>
       <p>Contact us for more info</p>
     </div>
--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -6,7 +6,8 @@ const Home = () => {
   return (
     <div>
       <Header />
-      <div className="px-[2em] py-[2em] h-[100dvh] grid grid-cols-4 gap-10">
+      <div className="px-[1em] py-[2em] h-[100dvh] grid grid-cols-4 gap-10">
+        {/* add your projects here bro */}
         <ProjectComp title="Quiz-2k25" projectSrc="/quiz-2k25.png" />
         <ProjectComp title="ChatMates" projectSrc="/ChatMates.png" />
       </div>
